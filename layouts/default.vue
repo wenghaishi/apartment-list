e22ceb8dd64423646e39eaa7dae2e30db81617fd
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-07-27 14:19:16 +0800

    created basic footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,41 @@
 <template>
-  <header class="shadow-sm bg-white">
-    <nav >
-      <div class="p-4 flex flex-row justify-between px-10 items-center h-16">
-        <div class="flex flex-row gap-10 items-center">
-          <NuxtLink to="/" class="flex flex-row gap-3 items-center"><img src="/house.svg" class="h-10 w-10"/>ApartmentPro</NuxtLink>
-          <NuxtLink to="/apartments">Apartments</NuxtLink>
-        </div>
+  <div class="h-screen flex flex-col justify-between">
+        <!-- header component -->
+    <header class="shadow-sm bg-white">
+      <nav >
+        <div class="p-4 flex flex-row justify-between px-10 items-center h-16">
+          <div class="flex flex-row gap-10 items-center">
+            <NuxtLink to="/" class="flex flex-row gap-3 items-center"><img src="/house.svg" class="h-10 w-10"/>ApartmentPro</NuxtLink>
+            <NuxtLink to="/apartments">Apartments</NuxtLink>
+          </div>
 
-        <div class="flex flex-row gap-2 items-center justify-center">
-          <input type="text" placeholder="Search" class="border-2 border-slate-500 focus:outline-none rounded-md p-2 h-8"/>
-          <img src="/search.svg" class="h-7 w-7"/>
-        </div>
+          <div class="flex flex-row gap-2 items-center justify-center">
+            <input type="text" placeholder="Search" class="border-2 border-slate-500 focus:outline-none rounded-md p-2 h-8"/>
+            <img src="/search.svg" class="h-7 w-7"/>
+          </div>
 
-        <div class="flex flex-row gap-6 items-center">
-          <NuxtLink to="login">Login</NuxtLink>
-          <NuxtLink class="bg-red-500 py-2 px-6 rounded-lg text-white" to="/signup">Get Started</NuxtLink>
+          <div class="flex flex-row gap-6 items-center">
+            <NuxtLink to="login">Login</NuxtLink>
+            <NuxtLink class="bg-red-500 py-2 px-6 rounded-lg text-white" to="/signup">Get Started</NuxtLink>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
-  <div>
-    <slot />
+      </nav>
+    </header>
+
+    <!-- body -->
+    <div>
+      <slot />
+    </div>
+
+    <!-- footer component -->
+    <div class="h-14 flex flex-row shadow-md items-center justify-between px-20 border-y">
+      <NuxtLink to="/" class="flex flex-row gap-3 items-center">ApartmentPro</NuxtLink>
+      <p>Copyright © 2023 - All right reserved by ApartmentPro Ltd</p>
+      <NuxtLink to="/apartments">Apartments</NuxtLink>
+    </div>
   </div>
+
+
 </template>
 
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
-<template>
+<template >
   <div class="flex flex-col items-center justify-evenly h-full mx-20 p-20 self-center">
-    <div class="flex flex-row justify-evenly items-center mb-10">
+    <div class="flex flex-row justify-evenly items-center mb-12">
       <h1 class="text-6xl font-semibold ">ApartmentPro</h1>
       <img src="house.svg" class="h-40 w-40"/>
     </div>
-    <h2 class="text-3xl mb-10">Streamlined Inventory Management for Apartments</h2>
+    <h2 class="text-3xl mb-12 underline-offset-2 italic">Streamlined Inventory Management for Apartments</h2>
     <p class="text-xl">Welcome to ApartmentPro - Your Comprehensive Inventory Management Solution for Apartments!
     ApartmentPro is a powerful web app designed to simplify and enhance the management of inventory 
     lists for apartments. With a sleek interface and intuitive features, it empowers users to efficiently 
